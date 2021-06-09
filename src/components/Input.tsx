5cbe96d4347b098ee5f76d240660812f773b78a0
limichange <limichange@hotmail.com>
2021-06-09 09:54:23 +0800

    update: empty component

---
+++ src/components/Input.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Input: React.FC<Props> = (props) => {
+  return <div>Input</div>
+}
+
+export default Input
---
+++ src/components/Modal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Modal: React.FC<Props> = (props) => {
+  return <div>Modal</div>
+}
+
+export default Modal
