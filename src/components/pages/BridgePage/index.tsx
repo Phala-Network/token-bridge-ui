e22cf153968be86a7ac12efc41215043058713b8
limichange <limichange@hotmail.com>
2021-06-22 18:26:50 +0800

    feat: step

--- src/components/pages/BridgePage/index.tsx
+++ src/components/pages/BridgePage/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BaseLayout from '../../BaseLayout'
 import InputDataStep from '../../BridgeModal/InputDataStep'
 import Spacer from '../../Spacer'
 import WhiteCard from '../../WhiteCard'
 import DetailTable from './DetailTable'
 import { Helmet } from 'react-helmet'
+import SubmitStep from '../../BridgeModal/SubmitStep'
+import ResultStep from '../../BridgeModal/ResultStep'
 
 type Props = {}
 
 const BridgePage: React.FC<Props> = () => {
+  const [step, setStep] = useState(0)
+
+  const next = () => {
+    setStep((prev) => prev + 1)
+  }
+
+  const prev = () => {
+    setStep((prev) => prev - 1)
+  }
+
   return (
     <BaseLayout>
       <Helmet>
@@ -16,7 +28,9 @@ const BridgePage: React.FC<Props> = () => {
       </Helmet>
       <div style={{ padding: 24, width: '100%' }}>
         <WhiteCard>
-          <InputDataStep onNext={() => {}}></InputDataStep>
+          {step === 0 && <InputDataStep onNext={next}></InputDataStep>}
+          {step === 1 && <SubmitStep onNext={next} onPrev={prev}></SubmitStep>}
+          {step === 2 && <ResultStep onNext={() => {}}></ResultStep>}
         </WhiteCard>
         <Spacer></Spacer>
         <WhiteCard>
