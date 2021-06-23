5b72aacebc3e0f4fd6466bcaa40c9b37b66fd517
limichange <limichange@hotmail.com>
2021-06-23 15:24:52 +0800

    feat: lang switch

--- src/components/SideNav/LangSwitch.tsx
+++ src/components/SideNav/LangSwitch.tsx
@@ -1,12 +1,61 @@
-import { IntlContext } from 'gatsby-plugin-intl'
+import { changeLocale, IntlContext } from 'gatsby-plugin-intl'
 import React, { useContext } from 'react'
+import styled from 'styled-components'
 
 type Props = {}
+
+const LangSwitchStyles = styled.div`
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  background: #373737;
+  padding: 6px;
+  width: 46px;
+`
+
+const LangSwitchButton = styled.button<{ active: boolean }>`
+  background-color: ${(props) => (props.active ? '#d1ff52' : 'transparent')};
+  color: ${(props) => (props.active ? 'black' : '#7A7A7A')};
+
+  border: none;
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 8px;
+  line-height: 20px;
+  height: 20px;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #d1ff52;
+    color: black;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+`
 
 const LangSwitch: React.FC<Props> = (props) => {
   const { locale } = useContext(IntlContext)
 
-  return <div>{locale}</div>
+  return (
+    <LangSwitchStyles>
+      <LangSwitchButton
+        onClick={() => changeLocale('en')}
+        active={locale === 'en'}
+      >
+        En
+      </LangSwitchButton>
+      <LangSwitchButton
+        onClick={() => changeLocale('zh')}
+        active={locale === 'zh'}
+      >
+        中
+      </LangSwitchButton>
+    </LangSwitchStyles>
+  )
 }
 
 export default LangSwitch
