d5d709c2068194bcce5f95d564a37425dfa338ad
limichange <limichange@hotmail.com>
2021-06-25 15:12:21 +0800

    chore: clear InputDataStep code

--- src/components/BridgeModal/InputDataStep.tsx
+++ src/components/BridgeModal/InputDataStep.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputAction from '../InputAction'
 import InputExternalInfo from '../InputExternalInfo'
 import InputNumber from '../InputNumber'
@@ -6,6 +6,7 @@ import Spacer from '../Spacer'
 import TradeTypeSelect from '../TradeTypeSelect'
 import Button from '../Button'
 import { ModalAction, ModalActions } from '../Modal'
+import { useEffect } from 'react'
 
 type Props = {
   onNext: () => void
@@ -13,35 +14,45 @@ type Props = {
 
 const InputDataStep: React.FC<Props> = (props) => {
   const { onNext } = props
+  const [balanceNumber, setBalanceNumber] = useState(0)
+
+  useEffect(() => {
+    setBalanceNumber(1234.5678)
+  }, [])
 
   return (
     <div>
       <TradeTypeSelect></TradeTypeSelect>
-      <Spacer></Spacer>
+
+      <Spacer />
+
       <InputNumber
-        size='large'
-        placeholder='Destination Address'
+        size="large"
+        placeholder="Destination Address"
         after={<InputAction>MY ADDRESS</InputAction>}></InputNumber>
-      <Spacer></Spacer>
+
+      <Spacer />
+
       <InputNumber
-        size='large'
-        placeholder='Amount (PHA)'
+        size="large"
+        placeholder="Amount (PHA)"
         after={<InputAction>MAX</InputAction>}></InputNumber>
+
       <Spacer y={0.2}></Spacer>
+
       <InputExternalInfo
         style={{ textAlign: 'right' }}
-        {...{
-          label: 'Balance',
-          value: 1234.56789,
-          type: 'PHA',
-        }}
+        label={'Balance'}
+        value={balanceNumber}
+        type={'PHA'}
       />
+
       <ModalActions>
         <ModalAction>
           <Button>Cancel</Button>
         </ModalAction>
         <ModalAction>
-          <Button type='primary' onClick={onNext}>
+          <Button type="primary" onClick={onNext}>
             Next
           </Button>
         </ModalAction>
