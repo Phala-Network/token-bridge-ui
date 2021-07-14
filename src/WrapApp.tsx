c9390e3e64380da2e7c094074feb2bd08f6cec99
limichange <limichange@hotmail.com>
2021-07-14 15:02:57 +0800

    chore: remove unused deps

--- src/WrapApp.tsx
+++ src/WrapApp.tsx
@@ -10,39 +10,30 @@ import { Web3Provider as PolkadotWeb3Provider } from './libs/polkadot/hooks/useW
 import { Provider as JotaiProvider } from 'jotai'
 import { ApiPromiseProvider } from './libs/polkadot/hooks/useApiPromise'
 import { NetworkContextProvider } from './libs/polkadot/hooks/useSubstrateNetwork'
-import { Provider as StyletronProvider } from 'styletron-react'
-import { LightTheme, BaseProvider } from 'baseui'
-import { Client as Styletron } from 'styletron-engine-atomic'
 import './fonts.css'
-
-const engine = new Styletron()
 
 const WrapApp: React.FC = ({ children }) => {
   const client = useRef(new QueryClient())
 
   return (
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <QueryClientProvider client={client.current}>
-          <EthereumWeb3Provider>
-            <JotaiProvider>
-              <EthersProvider>
-                <NetworkContextProvider defaultNetwork="poc4-dev">
-                  <PolkadotWeb3Provider originName="ChainBridge Operator">
-                    <ApiPromiseProvider>
-                      <ThemeProvider theme={theme}>
-                        <GlobalStyle></GlobalStyle>
-                        {children}
-                      </ThemeProvider>
-                    </ApiPromiseProvider>
-                  </PolkadotWeb3Provider>
-                </NetworkContextProvider>
-              </EthersProvider>
-            </JotaiProvider>
-          </EthereumWeb3Provider>
-        </QueryClientProvider>
-      </BaseProvider>
-    </StyletronProvider>
+    <QueryClientProvider client={client.current}>
+      <EthereumWeb3Provider>
+        <JotaiProvider>
+          <EthersProvider>
+            <NetworkContextProvider defaultNetwork="poc4-dev">
+              <PolkadotWeb3Provider originName="ChainBridge Operator">
+                <ApiPromiseProvider>
+                  <ThemeProvider theme={theme}>
+                    <GlobalStyle></GlobalStyle>
+                    {children}
+                  </ThemeProvider>
+                </ApiPromiseProvider>
+              </PolkadotWeb3Provider>
+            </NetworkContextProvider>
+          </EthersProvider>
+        </JotaiProvider>
+      </EthereumWeb3Provider>
+    </QueryClientProvider>
   )
 }
 
