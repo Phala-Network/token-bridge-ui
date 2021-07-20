cf45ba21f791d5c4f2593c6cf7467f81d7e32ed7
limichange <limichange@hotmail.com>
2021-07-20 15:46:38 +0800

    feat: transaction panel

--- src/components/transactions/Transactions.tsx
+++ src/components/transactions/Transactions.tsx
@@ -1,7 +1,64 @@
 import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 import useClickAway from '../../hooks/useClickAway'
-import TransactionsHeader from './TransactionsHeader'
+import ClearButton from './ClearButton'
+import TransactionsHeader from './Header'
+import TransactionsList from './List/List'
+
+const TEST_DATA = [
+  {
+    status: 'success',
+    from: {
+      network: 'ethereum',
+      address: '0xo298ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+    to: {
+      network: 'khala',
+      address: 'ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+  },
+  {
+    status: 'success',
+    from: {
+      network: 'ethereum',
+      address: '0xo298ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+    to: {
+      network: 'khala',
+      address: 'ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+  },
+  {
+    status: 'pending',
+    from: {
+      network: 'ethereum',
+      address: '0xo298ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+    to: {
+      network: 'khala',
+      address: 'ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+  },
+  {
+    status: 'pending',
+    from: {
+      network: 'ethereum',
+      address: '0xo298ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+    to: {
+      network: 'khala',
+      address: 'ofhwoehefo982i9w3fh',
+      amount: 123,
+    },
+  },
+]
 
 const TransactionsRoot = styled.div`
   position: fixed;
@@ -9,14 +66,12 @@ const TransactionsRoot = styled.div`
   height: 44px;
   right: 48px;
   bottom: 48px;
-
+  transform: translate3d(0, 0, 0);
   background: #ffffff;
   border: 1px solid #000000;
   box-sizing: border-box;
-
   z-index: 2;
-
-  transition: all 0.3s ease;
+  transition: all 0.2s ease;
 
   &.active {
     width: 410px;
@@ -28,6 +83,7 @@ const TransactionsRoot = styled.div`
 const Transactions: React.FC = () => {
   const [active, setActive] = useState(false)
   const rootRef = useRef(null)
+  const [data, setData] = useState(TEST_DATA)
 
   useClickAway(rootRef, () => {
     setActive(false)
@@ -35,7 +91,17 @@ const Transactions: React.FC = () => {
 
   return (
     <TransactionsRoot ref={rootRef} className={active ? 'active' : ''}>
-      <TransactionsHeader onClick={() => setActive(true)}></TransactionsHeader>
+      <TransactionsHeader
+        active={active}
+        onClickHeader={() => setActive((prev) => !prev)}
+      />
+
+      {active && (
+        <>
+          <ClearButton onClick={() => setData([])}>Clear</ClearButton>
+          <TransactionsList transactions={data} />
+        </>
+      )}
     </TransactionsRoot>
   )
 }
