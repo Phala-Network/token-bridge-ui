b792252c1d88f9880282745b0c1984c039f4263b
limichange <limichange@hotmail.com>
2021-07-29 10:07:30 +0800

    chore: upgrade deps

--- src/components/BalanceLabel.tsx
+++ src/components/BalanceLabel.tsx
@@ -4,16 +4,19 @@ import React from 'react'
 
 type Props = {
   value: Decimal
+  precision: number
   type?: string
 }
 
 const BalanceLabel: React.FC<Props> = (props) => {
-  const { value, type = '' } = props
+  const { precision = 4, value, type = '' } = props
 
   const balanceDisplay =
     !value || value.lessThan(new Decimal(0))
       ? '...'
-      : currency(value.toString(), { symbol: '' }).format().toString()
+      : currency(value.toString(), { symbol: '', precision })
+          .format()
+          .toString()
 
   const result = `${balanceDisplay} ${type}`.trim()
 
