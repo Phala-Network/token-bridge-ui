81bd71b2e7ebc720d7759319663b8ceee77c692d
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2021-07-29 17:49:18 +0800

    fix: global style

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 const React = require('react')
 const App = require('./src/WrapApp').default
+const WrapPage = require('./src/WrapPage').default
 
 exports.wrapRootElement = ({ element }) => {
   return <App>{element}</App>
 }
+
+exports.wrapPageElement = ({ props, element }) => (
+  <WrapPage {...props}>{element}</WrapPage>
+)
