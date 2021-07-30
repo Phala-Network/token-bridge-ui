224d9ad146f24c207b10c5ecd749c18a84167790
limichange <limichange@hotmail.com>
2021-07-31 00:00:48 +0800

    Update

--- src/config.ts
+++ src/config.ts
@@ -9,10 +9,10 @@ import {
  * Throws unsupported network on not defined networks in the following table
  */
 export const ethereums: Record<number, EthereumNetworkOptions> = {
-  42: {
-    bridge: '0xe5F54e020f3E4964Ba11D269Cdda602A78d09917',
-    erc20: '0x512f7a3c14b6ee86c2015bc8ac1fe97e657f75f2',
-    erc20AssetHandler: '0xDf2E83f33dB8A9CcF3a00FCe18C3F509b974353D',
+  1: {
+    bridge: '0xf88337a0db6e24Dff0fCD7F92ab0655B97A68d38',
+    erc20: '0x6c5ba91642f10282b576d91922ae6448c9d52f4e',
+    erc20AssetHandler: '0x510EED5833f9aCf5b7a66Ef391eA5118b84b86f2',
     erc20ResourceId:
       '0x00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001',
     peerChainIds: {
@@ -26,7 +26,7 @@ export const substrates: Record<string, SubstrateNetworkOptions> = {
     endpoint:
       process.env['PHALA_ENDPOINT'] ?? 'wss://pc-test-2.phala.network/khala/ws',
     peerChainIds: {
-      42: 0,
+      1: 0,
     },
     typedefs: khalaDev,
   },
