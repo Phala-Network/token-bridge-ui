d4785bb90b86da02d7100000b27b9cad087836e4
limichange <limichange@hotmail.com>
2021-08-02 15:24:16 +0800

    feat: mobile style

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -6,12 +6,12 @@ const Address = styled.div`
   font-weight: normal;
   font-size: 12px;
   line-height: 13px;
-  display: flex;
-  align-items: center;
-  text-align: center;
   letter-spacing: 1px;
   color: #494949;
   padding: 6px 0;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  hyphens: auto;
 `
 
 export default Address
