14c6ff6cc976c89f1affcd411cdc5d0efe1af95d
limichange <limichange@hotmail.com>
2021-08-05 14:46:39 +0800

    fix: allowance check

--- src/components/bridge/EthereumAllowance.tsx
+++ src/components/bridge/EthereumAllowance.tsx
@@ -60,7 +60,7 @@ const EthereumAllowance: FC<Props> = (props: Props) => {
     }
   }
 
-  return transactionsInfoSuccess
+  return (allowanceText && allowanceText !== '0.0') || transactionsInfoSuccess
     ? children
     : cloneElement(placeholder, {
         onClick: startApprove,
